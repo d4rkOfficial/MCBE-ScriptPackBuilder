4aedfe063f74679c90d9917cd36dd7533e0f03f3
d4rK6666@outlook.com <d4rK6666@outlook.com>
2022-12-04 17:28:56 +0800

    .

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,12 +1,20 @@
-const helloWorld = `import * as mc from "@minecraft/server"
-let time = 0
-let timer = mc.world.events.tick.subscribe(_ => {
-	if (++time === 300) {
-		mc.world
-		  .getDimension("overworld")
-		  .runCommand("say Hello, world!")
+const helloWorld = `import { world, system } from "@minecraft/server"
+let tickIndex = 0
+
+system.run(function mainTick() {
+	try {
+		tickIndex ++
+		if (tickIndex === 300) {
+			world.getDimension('overworld')
+			  .runCommandAsync('Hello, world!')
+		}
+	} catch (error) {
+		console.warn('Error: ' + error)
 	}
+	system.run(mainTick)
 })
+
+
 `
 
 /*
