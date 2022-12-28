36d05a12bf36474a35e0b9ca116336be43ed51bb
d4rK6666@outlook.com <d4rK6666@outlook.com>
2022-12-28 16:10:32 +0800

    .

--- src/@main.js
+++ src/@main.js
@@ -5,9 +5,9 @@ import { runCommand, downloadIcon, randomUUID, printLogo } from './utils.js'
 import { createHelloWorldJS } from './helloworld.js'
 
 (async function main() {
-	
+
 	const server = false
-	
+
 	const isEmptyFolder = await testEmptyFolder()
 	if (!isEmptyFolder) {
 		print('You should initialize your project in an empty folder!')
@@ -35,9 +35,9 @@ import { createHelloWorldJS } from './helloworld.js'
 			'(3) 1.16.210+', // -> 1 | 2 | 3
 			'Default: (1)'
 	], {
-		DEFAULT: 1 //latest engine version as default
-	})
-	
+			DEFAULT: 1 //latest engine version as default
+		})
+
 	if (!([1, 2].includes(mcbe_vers))) {
 		print('This version is not temporarily supported.')
 		//versions under 1.19.30.25 is not supported
@@ -46,18 +46,18 @@ import { createHelloWorldJS } from './helloworld.js'
 
 	print('\x1B[37mGenerating...')
 
-  print('\x1B[32mSetting up scripts/index.js')
+	print('\x1B[32mSetting up scripts/index.js')
 	await createFolder('scripts')
 	await createHelloWorldJS(mcbe_vers)
 	print('\x1B[37mdone.')
-	
+
 	print('\x1B[32mSetting up manifest.json')
 	let headerUUID, moduleUUID
 	await createManifest(mcbe_vers, pack_name, pack_desc, pack_auth,
-	  headerUUID = randomUUID(),
-	  moduleUUID = randomUUID())
+		headerUUID = randomUUID(),
+		moduleUUID = randomUUID())
 	print('\x1B[37mdone.')
-	
+
 	print('\x1B[32mDownloading pack_icon.png')
 	try {
 		await downloadIcon()
@@ -65,17 +65,18 @@ import { createHelloWorldJS } from './helloworld.js'
 	} catch {
 		print('\x1B[31mNetwork Error: Unable to download default pack_icon.png')
 	}
-	
+
 	print('\x1B[32mInstalling Type Docs')
 	try {
-	await runCommand('npm i @minecraft/server@1.1.0-beta.1.19.60-preview.24')
-	await runCommand('npm i @minecraft/server-gametest@1.0.0-beta.1.19.60-preview.24')
-	await runCommand('npm i npm i @minecraft/server-ui@1.0.0-beta.1.19.60-preview.24')
-	
-	if (server) { /*#__pure__*/
-	  await runCommand('npm i @minecraft/server-net@latest')
-	  await runCommand('npm i @minecraft/server-admin@latest')
-	}
+		await runCommand('npm i @minecraft/server@1.1.0-beta.1.19.60-preview.24')
+		await runCommand('npm i @minecraft/server-gametest@1.0.0-beta.1.19.60-preview.24')
+		await runCommand('npm i @minecraft/server-ui@1.0.0-beta.1.19.60-preview.24')
+
+		if (server) {
+			/*#__pure__*/
+			await runCommand('npm i @minecraft/server-net@latest')
+			await runCommand('npm i @minecraft/server-admin@latest')
+		}
 	} catch {}
 
 	print('\x1B[37mFinished! \n')
